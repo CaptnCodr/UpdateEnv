374bdcf6a3dc55927a567806d32c46616e8894c7
Constantin Tews <constantin.tews@gmail.com>
2023-01-17 15:12:23 +0100

    Add code and some docs.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# UpdateEnv
+# Update-Env "Variable"
+
+Update an already loaded environment variable with this PowerShell cmdlet in only one short line:
+`Update-Env "YourVar"`
+
+more is coming ...
---
+++ UpdateEnvironmentVariable.fs
@@ -0,0 +1,31 @@
+﻿namespace UpdateEnv
+
+open System
+open System.Management.Automation
+
+[<Cmdlet("Update", "Env")>]
+type UpdateEnvironmentVariable () =
+    inherit PSCmdlet ()
+
+    [<Parameter(Position = 0, Mandatory = true)>]
+    member val Var : string = "" with get, set
+
+    override this.ProcessRecord () =
+        let psVar =
+            this.SessionState.InvokeProvider.Item.Get($@"Env:\{this.Var}")
+            |> Seq.head
+            |> fun o -> o.Properties
+            |> Seq.tryFind (fun p -> p.Name = "Value")
+            |> function
+                | Some prop -> $"PS: {prop.Value}"
+                | None -> "PS: "
+
+        let envVar = $"Env: {Environment.GetEnvironmentVariable(this.Var, EnvironmentVariableTarget.User)}"
+
+        this.WriteObject ($"{psVar}, {envVar}")
+
+        ($@"Env:\{this.Var}", Environment.GetEnvironmentVariable(this.Var, EnvironmentVariableTarget.User))
+        |> this.SessionState.InvokeProvider.Item.Set
+        |> ignore
+
+        this.WriteObject ($"'{this.Var}' has been updated!")
