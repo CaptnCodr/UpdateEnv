024000c3ea99877a87e259e166cc5e8fb01406fd
Constantin Tews <constantin.tews@gmail.com>
2023-01-20 22:56:48 +0100

    Don't emit any text to user.

--- UpdateEnvironmentVariable.fs
+++ UpdateEnvironmentVariable.fs
@@ -11,21 +11,6 @@ type UpdateEnvironmentVariable () =
     member val Var : string = "" with get, set
 
     override this.ProcessRecord () =
-        let psVar =
-            this.SessionState.InvokeProvider.Item.Get($@"Env:\{this.Var}")
-            |> Seq.head
-            |> fun o -> o.Properties
-            |> Seq.tryFind (fun p -> p.Name = "Value")
-            |> function
-                | Some prop -> $"PS: {prop.Value}"
-                | None -> "PS: "
-
-        let envVar = $"Env: {Environment.GetEnvironmentVariable(this.Var, EnvironmentVariableTarget.User)}"
-
-        this.WriteObject ($"{psVar}, {envVar}")
-
         ($@"Env:\{this.Var}", Environment.GetEnvironmentVariable(this.Var, EnvironmentVariableTarget.User))
         |> this.SessionState.InvokeProvider.Item.Set
         |> ignore
-
-        this.WriteObject ($"'{this.Var}' has been updated!")
